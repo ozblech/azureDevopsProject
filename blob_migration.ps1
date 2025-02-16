d21bdd812070c56443d38edafa3f3607ba8ed567
Oz Blech <ozblech87@gmail.com>
2025-02-16 17:24:47 +0200

    sleep between deleting and creating containers

--- blob_migration.ps1
+++ blob_migration.ps1
@@ -19,6 +19,9 @@ Write-Host "Deleting and recreating the container in both storage accounts..."
 az storage container delete --name $containerName --account-name $sourceStorageAccount --auth-mode login
 az storage container delete --name $containerName --account-name $destinationStorageAccount --auth-mode login
 
+# Wait for a few seconds to ensure the container is fully deleted
+Start-Sleep -Seconds 10
+
 # Recreate the containers after deletion
 Write-Host "Recreating the containers..."
 az storage container create --name $containerName --account-name $sourceStorageAccount --auth-mode login
