6c6511a758b719381b1d36a6d09aa79416f1e859
Oz Blech <ozblech87@gmail.com>
2025-02-17 17:56:07 +0200

    fix

--- blob_migration.ps1
+++ blob_migration.ps1
@@ -34,11 +34,11 @@ for ($i = 1; $i -le 5; $i++) {
     $filePath = "$tempDir/file$i.txt"  # Using forward slash for Ubuntu compatibility
     "This is test file $i" | Out-File -FilePath $filePath
 
-    az storage blob upload `
-        --account-name $sourceStorageAccount `
-        --container-name $containerName `
-        --file $filePath `
-        --name "file$i.txt" `
+    az storage blob upload \
+        --account-name $sourceStorageAccount \
+        --container-name $containerName \
+        --file $filePath \
+        --name "file$i.txt" \
         --auth-mode login --debug
     if ($?) {
         Write-Host "Blob file$i.txt uploaded successfully"
